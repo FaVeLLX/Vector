67d0eede3c3a838ad8403da086df61fdada1652a
FaVeLLX <m9117007835@mail.ru>
2026-08-02 16:54:56 +0300

    1

--- timeweb-server/server.js
+++ timeweb-server/server.js
@@ -129,3 +129,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+    res.status(200).send('ok');
+});
