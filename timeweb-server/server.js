175949f10fd9071a110aba00ab160f1e51d3496e
FaVeLLX <m9117007835@mail.ru>
2026-08-02 17:31:00 +0300

    ласт

--- timeweb-server/server.js
+++ timeweb-server/server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+    next();
+});
+
 const path = require('path');
 app.use(express.static(path.join(__dirname, '.')));
 
